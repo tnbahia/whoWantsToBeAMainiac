865989841d8f0672bf4f7c4aa03f50c190ee5c1c
tnbahia <tnbahia@gmail.com>
2019-03-15 19:54:06 +0000

    work on gameServer

--- src/org/academiadecodigo/mainiacs/GameServer.java
+++ src/org/academiadecodigo/mainiacs/GameServer.java
@@ -1,11 +1,34 @@
 package org.academiadecodigo.mainiacs;
 
+import org.academiadecodigo.mainiacs.controllers.LoginController;
+
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class GameServer {
-    private ServerSocket serverSocket;
+    public static final int DEFAULT_PORT = 8080;
 
     public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT);
+            LoginController loginController = GameSetup.setup();
 
+            int players = 0;
+            while (players < 4) {
+                Socket newPlayer = serverSocket.accept();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        loginController.newPlayer();
+                    }
+                }).start();
+                players++;
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
