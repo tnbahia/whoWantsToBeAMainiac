4b497e89db054127acede29a7495ddca08b780f4
DiefersonMatos <diefersonamatos@gmail.com>
2019-03-16 18:47:09 +0000

    update Game
    inserir assinatura de alguns metodos para poder iniciar os testes

--- src/org/academiadecodigo/mainiacs/domains/Game.java
+++ src/org/academiadecodigo/mainiacs/domains/Game.java
@@ -6,4 +6,22 @@ import java.util.LinkedList;
 public class Game {
     private HashMap<String,Player> players;
     private LinkedList<Integer> questionsAsked;
+
+    public String[] getOptions(){
+        String[] options = Questions.values()[0].getOptions();
+        return options;
+    }
+
+    public void checkAnwser(int answer){
+
+    }
+
+    public String getQuestion(){
+        String question = Questions.values()[0].getQuestion();
+        return question;
+    }
+
+    public void addPlayer(Player player){
+
+    }
 }
