2c592756196bc1cdbe9c5119a4f80c94fed9fac9
franciscopls <franciscopireslimasilva@gmail.com>
2019-03-17 16:41:11 +0000

    Adding some logo messages & messages correction

--- src/org/academiadecodigo/mainiacs/domains/utils/Messages.java
+++ src/org/academiadecodigo/mainiacs/domains/utils/Messages.java
@@ -6,7 +6,15 @@ package org.academiadecodigo.mainiacs.domains.utils;
  */
 
 public class Messages {
-    
+
+    public static final String LOGO = "\n" + "WHO WANTS TO BE A \n" +
+            ".88b  d88.  .d8b.  d8b   db d888888b  .d8b.   .o88b. \n" +
+            "88'YbdP`88 d8' `8b 888o  88   `88'   d8' `8b d8P  Y8 \n" +
+            "88  88  88 88ooo88 88V8o 88    88    88ooo88 8P      \n" +
+            "88  88  88 88~~~88 88 V8o88    88    88~~~88 8b      \n" +
+            "88  88  88 88   88 88  V888   .88.   88   88 Y8b  d8 \n" +
+            "YP  YP  YP YP   YP VP   V8P Y888888P YP   YP  `Y88P' \n\n" +
+            "*****************************************************\n";
     public static final String WELCOME = "Welcome to Who Wants To Be A #MAINiac! \n " +
             "A game about Java, the MAINiacs and the wonderful world of programming.";
     public static final String LOGIN_PLAYER = "What do you want us to call you? ";
